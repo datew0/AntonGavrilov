5265c07a6fb5f148fc75799de62b5e7afa122eb3
datew0 <44378542+datew0@users.noreply.github.com>
2022-04-16 19:36:23 +0300

    Driver configuration added

--- src/test/java/hw2/SeleniumTest.java
+++ src/test/java/hw2/SeleniumTest.java
@@ -4,13 +4,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
+
+import java.time.Duration;
 
 public abstract class SeleniumTest {
     protected WebDriver driver;
 
-    public SeleniumTest() {
+    @BeforeSuite
+    public void initSuite() {
         String driverPath = getClass().getClassLoader().getResource("chromedriver").getPath();
         System.setProperty("webdriver.chrome.driver", driverPath);
     }
@@ -18,6 +21,8 @@ public abstract class SeleniumTest {
     @BeforeTest
     public void init() {
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
 
     @AfterMethod
