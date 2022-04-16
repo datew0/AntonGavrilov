548c2d3c1ade3d965595a98fc99cb3f32120bf7f
datew0 <44378542+datew0@users.noreply.github.com>
2022-04-16 20:15:17 +0300

    Moved to flexible locators

--- src/test/java/hw2/ex2/ControlsTest.java
+++ src/test/java/hw2/ex2/ControlsTest.java
@@ -8,6 +8,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
+import java.util.List;
+
 public class ControlsTest extends SeleniumTest {
 
     @Test
@@ -35,16 +37,16 @@ public class ControlsTest extends SeleniumTest {
         driver.findElement(By.cssSelector("ul.dropdown-menu > li > a[href=\"different-elements.html\"]")).click();
 
         // Select checkboxes: Water, Wind
-        WebElement waterCBox = driver.findElement(By.xpath("/html/body/div[1]/div[2]/main[1]/div[2]/div[1]/div[2]/label[1]/input[1]"));
+        WebElement waterCBox = driver.findElement(By.xpath("//label[contains(.,\"Water\")]/input"));
         waterCBox.click();
         softAssert.assertTrue(waterCBox.isSelected());
-        WebElement windCBox = driver.findElement(By.xpath("/html/body/div[1]/div[2]/main[1]/div[2]/div[1]/div[2]/label[3]/input[1]"));
+        WebElement windCBox = driver.findElement(By.xpath("//label[contains(.,\"Wind\")]/input"));
         windCBox.click();
         softAssert.assertTrue(windCBox.isSelected());
         int a = 10;
 
         // Select radio: Selen
-        WebElement selenRadioBtn = driver.findElement(By.xpath("/html/body/div[1]/div[2]/main[1]/div[2]/div[1]/div[3]/label[4]/input[1]"));
+        WebElement selenRadioBtn = driver.findElement(By.xpath("//label[contains(.,\"Selen\")]/input"));
         selenRadioBtn.click();
         softAssert.assertTrue(selenRadioBtn.isSelected());
 
@@ -54,12 +56,12 @@ public class ControlsTest extends SeleniumTest {
         softAssert.assertEquals(dropdownMenu.getFirstSelectedOption().getText(), "Yellow");
 
         // Assert that for each checkbox there is an individual log row and value is corresponded to the status of checkbox
-        WebElement log[] = new WebElement[4];
-        for(int i=0; i<4; ++i)
-            log[i] = driver.findElement(By.xpath("//*[@id=\"mCSB_2_container\"]/section[1]/div[2]/div[1]/ul[1]/li[" + (i+1) + "]"));
-        softAssert.assertTrue(log[0].getText().contains("Yellow"));
-        softAssert.assertTrue(log[1].getText().contains("Selen"));
-        softAssert.assertTrue(log[2].getText().contains("Wind: condition changed to true"));
-        softAssert.assertTrue(log[3].getText().contains("Water: condition changed to true"));
+        List<WebElement> log = driver.findElements(By.cssSelector("ul.logs > li"));
+        String expectedLogMsg[] = new String[] {"Yellow", "Selen", "Wind", "Water"};
+        for (int i=0; i < 4; ++i){
+            softAssert.assertTrue(log.get(i).getText().contains(expectedLogMsg[i]));
+        }
+
+        softAssert.assertAll();
     }
 }
