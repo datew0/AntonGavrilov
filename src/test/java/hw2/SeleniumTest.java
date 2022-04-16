b596079b30df6483e8d427e4c68c63afefdf5e97
datew0 <44378542+datew0@users.noreply.github.com>
2022-04-16 20:45:51 +0300

    Driver init order changed

--- src/test/java/hw2/SeleniumTest.java
+++ src/test/java/hw2/SeleniumTest.java
@@ -3,9 +3,8 @@ package hw2;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
 
 import java.time.Duration;
 
@@ -18,7 +17,7 @@ public abstract class SeleniumTest {
         System.setProperty("webdriver.chrome.driver", driverPath);
     }
 
-    @BeforeTest
+    @BeforeMethod
     public void init() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -27,13 +26,7 @@ public abstract class SeleniumTest {
 
     @AfterMethod
     public void clean() {
-        driver.close();
-    }
-
-    @AfterTest
-    public void exit() {
         driver.quit();
         driver = null;
     }
-
 }
