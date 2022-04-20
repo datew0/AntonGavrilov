ad8ee1cf1756e5a1467b25dbcfd60558e9845b0d
datew0 <44378542+datew0@users.noreply.github.com>
2022-04-20 10:19:08 +0300

    Reading properties from file

--- src/test/java/hw3/SeleniumTest.java
+++ src/test/java/hw3/SeleniumTest.java
@@ -7,14 +7,23 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 public abstract class SeleniumTest {
     protected WebDriver driver;
+    protected Properties properties = new Properties();
 
     @BeforeSuite
     public void initSuite() {
         WebDriverManager.chromedriver().setup();
+        try {
+            properties.load(new FileReader("src/test/resources/user.properties"));
+        } catch (IOException e) {
+            System.err.println("Loading properties from file failed!");
+        }
     }
 
     @BeforeMethod
